7dacda38963e8da26e1d705e32650e4d0337b619
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-06-25 17:32:34 -0600

    Update fix_updates.bat

--- fix_updates.bat
+++ fix_updates.bat
@@ -1,1 +1,8 @@
 #Batch
+
+#Windows Software Probe
+#Windows Agent
+
+"C:\Program Files (x86)\SolarWinds MSP\CacheService\unins000.exe" /silent
+"C:\Program Files (x86)\SolarWinds MSP\PME\unins000.exe" /silent
+"C:\Program Files (x86)\SolarWinds MSP\RpcServer\unins000.exe" /silent
