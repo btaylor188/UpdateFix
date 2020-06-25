667d5e755a61b54c655c23cb7c74c1958c71c76a
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-06-25 17:39:51 -0600

    Update fix_updates.bat

--- fix_updates.bat
+++ fix_updates.bat
@@ -3,5 +3,5 @@
 "C:\Program Files (x86)\SolarWinds MSP\CacheService\unins000.exe" /silent
 "C:\Program Files (x86)\SolarWinds MSP\PME\unins000.exe" /silent
 "C:\Program Files (x86)\SolarWinds MSP\RpcServer\unins000.exe" /silent
-wmic product where name ="Windows Software Probe" call uninstall /nointeractive
-wmic product where name ="Windows Agent" call uninstall /nointeractive 
+wmic product where name="Windows Software Probe" call uninstall /nointeractive
+wmic product where name="Windows Agent" call uninstall /nointeractive 
