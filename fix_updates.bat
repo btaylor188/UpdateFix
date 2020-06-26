f2e3c7943ebe558b34f184ef6042d199f5b29faf
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-06-26 16:34:09 -0600

    Update fix_updates.bat

--- fix_updates.bat
+++ fix_updates.bat
@@ -1,7 +1,12 @@
 @echo off
 
+#Remove Solar Winds Software
+echo "Removing Solarwinds Software"
+echo "Removing MSP Cache Service"
 "C:\Program Files (x86)\SolarWinds MSP\CacheService\unins000.exe" /silent
 "C:\Program Files (x86)\SolarWinds MSP\PME\unins000.exe" /silent
 "C:\Program Files (x86)\SolarWinds MSP\RpcServer\unins000.exe" /silent
 wmic product where name="Windows Software Probe" call uninstall /nointeractive
 wmic product where name="Windows Agent" call uninstall /nointeractive 
+
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate
