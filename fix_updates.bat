90c0af662980d5b73feed4bb7927d4e581ec913b
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-06-30 17:05:52 -0600

    Update fix_updates.bat

--- fix_updates.bat
+++ fix_updates.bat
@@ -1,5 +1,5 @@
 @echo off
-#Remove Solar Winds Software
+rem Remove Solar Winds Software
 echo "Removing Solarwinds Software"
 echo "Removing MSP Cache Service"
 "C:\Program Files (x86)\SolarWinds MSP\CacheService\unins000.exe" /silent
@@ -15,5 +15,13 @@ echo "Removing Windows Update Registry Keys"
 reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f
 echo "Updating Group Policy"
 gpupdate /force
+echo "Schedule Reboot?"
+choice 
+if errorlevel 1 goto reboot
+if errorlevel 2 goto exit
+:reboot
 echo "Scheduling Reboot at 11:59 PM"
-schtasks /create /sc once /tn "Reboot" /tr "shutdown -r" /st 23:59
+rem schtasks /create /sc once /tn "Reboot" /tr "shutdown -r" /st 23:59
+
+:exit
+exit
